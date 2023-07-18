61c002f396ac12f0ede999cd98f9db7c1423e846
khaledrokaya <112181079+khaledrokaya@users.noreply.github.com>
2023-07-19 00:55:19 +0300

    Update Longest_Substring_Without_Repeating_Characters.cpp

--- Longest_Substring_Without_Repeating_Characters.cpp
+++ Longest_Substring_Without_Repeating_Characters.cpp
@@ -1,29 +1,26 @@
-#include <iostream>
-#include <unordered_set>
-// #include <algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 int lengthOfLongestSubstring(string s) {
-      unordered_set<char> uniqueChars;
-      int maxLength = 0;
-      int left = 0, right = 0;
-      while (right < s.length()) {
-            if (uniqueChars.find(s[right]) == uniqueChars.end()) {
-                  uniqueChars.insert(s[right]);
-                  maxLength = max(maxLength, right - left + 1);
-                  right++;
-            }
-            else {
-                  uniqueChars.erase(s[left]);
-                  left++;
-            }
-      }
-      return maxLength;
+    int n = s.size();
+    unordered_map <char, int> mp;
+    int j = 0;
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        mp[s[i]]++;
+        while (mp[s[i]] == 2) { // if we met repeated char we will move forward untill we make all chars in window equal to 1 again
+            mp[s[j]]--;
+            if (mp[s[j]] == 0) // erase here just to reduce memory you can add or remove as you want
+                mp.erase(s[j]);
+            j++;
+        }
+        ans = max(ans, i - j + 1);
+    }
+    return ans;
 }
 int main() {
-      string s;
-      cout << "Enter a string: ";
-      cin >> s;
-      int longestLength = lengthOfLongestSubstring(s);
-      cout << "Length of the longest substring without repeating characters: " << longestLength << endl;
-      return 0;
+    string s;
+    cin >> s;
+    int longestLength = lengthOfLongestSubstring(s);
+    cout << longestLength << endl;
+    return 0;
 }
