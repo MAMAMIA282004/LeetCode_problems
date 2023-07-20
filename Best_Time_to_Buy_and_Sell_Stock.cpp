0ee1a854ea275216ad1813d2adee4cc10148285d
khaledRokaya <112181079+khaledrokaya@users.noreply.github.com>
2023-07-20 18:48:20 +0300

    Update Best_Time_to_Buy_and_Sell_Stock.cpp

--- Best_Time_to_Buy_and_Sell_Stock.cpp
+++ Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -1,15 +1,35 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main() {
-    int n;
-    cin >> n;
-    vector<int> nums(n);
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
+
+int maxProfit(vector<int>& prices) {
+    int n = prices.size();
+    if (n == 0) return 0; // If the array is empty, no profit can be made.
+
+    int maxProfit = 0;
+    int minPrice = prices[0]; // Initialize the minimum price as the first day's price.
+
+    for (int i = 1; i < n; i++) {
+        // Update the maximum profit if the current day's price minus the minimum price is greater.
+        maxProfit = max(maxProfit, prices[i] - minPrice);
+
+        // Update the minimum price if the current day's price is smaller.
+        minPrice = min(minPrice, prices[i]);
     }
-    auto pair = minmax_element(nums.begin(), nums.end());
-    int result = *pair.second - *pair.first;
-    cout << result << endl;
+
+    return maxProfit;
+}
+
+int main() {
+    // Test cases
+    vector<int> prices1 = {7, 1, 5, 3, 6, 4};
+    cout << "Max profit from buying and selling stock 1: " << maxProfit(prices1) << endl;
+
+    vector<int> prices2 = {7, 6, 4, 3, 1};
+    cout << "Max profit from buying and selling stock 2: " << maxProfit(prices2) << endl;
+
+    vector<int> prices3 = {3, 8, 2, 5, 1, 6};
+    cout << "Max profit from buying and selling stock 3: " << maxProfit(prices3) << endl;
+
     return 0;
 }
-// زق يومك متعقدهاش علي نفسك يعم (^_^)
